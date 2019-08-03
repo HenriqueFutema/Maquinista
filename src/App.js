a0fa0bae090ea7eeb486be06b5d147329256e646
HenriqueFutema <minifut0407@gmail.com>
2019-08-03 17:02:57 -0300

    Nav provisório

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
+import NavBar from "./components/NavBar";
 
 function App() {
-  
-
   return (
     <div className="App">
-      <h1>Test</h1>
+      <NavBar />
     </div>
   );
 }
