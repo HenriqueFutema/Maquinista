a0addf900b8129fd2a15330e04451d154db7391a
HenriqueFutema <minifut0407@gmail.com>
2019-08-09 18:02:13 -0300

    Imagens tests

--- src/components/NavBarComponent.js
+++ src/components/NavBarComponent.js
@@ -1,7 +1,5 @@
 import React from "react";
 
-// import { Container } from './styles';
-
 const NavBar = () => (
   <nav className="navbar navbar-expand-lg navbar-light bg-light">
     <a className="navbar-brand" href="#">
--- src/components/NossoObjetivoComponent.js
+++ src/components/NossoObjetivoComponent.js
@@ -1,33 +1,35 @@
 import React from "react";
 
 import Test from "../assets/note1.jpg";
+import Lampada from "../assets/lm2.jpg";
+import Formandos from "../assets/formandos.jpg";
 
 const NossoObjetivo = () => (
   <div className="container mt-5">
     <div className="row">
       <div className="col-12 col-lg-6">
         <div className="mt-5 text-right">
-          <h1 className="display-4">Nosso Objetivo</h1>
-          <p className="lead mt-3">
+          <h1 className="display-4 mt-5">Nosso Objetivo</h1>
+          <p className="lead mt-5">
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae iure,
             libero saepe eaque cupiditate accusantium eos quas sapiente illo
             assumenda quos enim
           </p>
         </div>
       </div>
-      <div className="col-12 col-lg-6">
+      <div className="col-12 col-lg-6 text-center">
         <div className="row">
-          <div className="col-12 col-lg-6 col-md-6">
-            <img src={Test} alt="" className="img-fluid" />
+          <div className="col-12 col-lg-6 col-md-6 p-0">
+            <img src={Lampada} alt="" className="img-fluid" />
           </div>
-          <div className="col-12 col-lg-6 col-md-6">
-            <img src={Test} alt="" className="img-fluid" />
+          <div className="col-12 col-lg-6 col-md-6 p-0">
+            <img src={Formandos} alt="" className="img-fluid" />
           </div>
-          <div className="col-12 col-lg-6 col-md-6">
-            <img src={Test} alt="" className="img-fluid" />
+          <div className="col-12 col-lg-6 col-md-6 p-0 mt-2">
+            <img src={Formandos} alt="" className="img-fluid" />
           </div>
-          <div className="col-12 col-lg-6 col-md-6">
-            <img src={Test} alt="" className="img-fluid" />
+          <div className="col-12 col-lg-6 col-md-6 p-0 mt-2">
+            <img src={Lampada} alt="" className="img-fluid" />
           </div>
         </div>
       </div>
