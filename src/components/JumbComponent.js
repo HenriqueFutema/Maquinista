edb453f6bb276766cefe791cba0881a6f815c873
HenriqueFutema <minifut0407@gmail.com>
2019-08-09 16:24:35 -0300

    Jumbotron

--- src/components/JumbComponent.js
+++ src/components/JumbComponent.js
@@ -3,10 +3,15 @@ import React from "react";
 export default function Jumb() {
   return (
     <div
-      className="container-fluid text-center bg-danger p-0 my-5
+      className="container-fluid text-center bg-danger p-0 my-5 jumb
     "
     >
-      <h1>O que voçê quer ser quando crescer</h1>
+      <div className="jumb-2">
+        <h1 className="display-2">
+          O que voçê quer ser <br />
+          quando crescer ?
+        </h1>
+      </div>
     </div>
   );
 }
